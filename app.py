6d077667586e9f42b1b4cac4d3d002f1dc437cf1
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-03-18 00:14:35 +0530

    Updated Website

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template,request
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def home():
+    return render_template("index.html")
+
+     
