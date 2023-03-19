9d9bcb27c4ba340aac2eb6c331d3b8cb181d1167
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-03-19 16:20:43 +0530

    Created pickle instance

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, request, render_template,request
-from markupsafe import escape
+import pickle
+import requests
+import pandas as pd
+from patsy import dmatrices
+
+movies=pickle.load(open('model/movies.pkl','rb'))
+def poster(movie_id):
+    url="https://api.themoviedb.org/3/movie/{}?api_key=906b44d502c9c4a91034067e114671a3",format(movie_id)
+    data=requests.get(url)
+    data=data.json()
+    poster_path=data['poster_path']
+    full_path="https://image.tmdb.org/t/p/w500/"+ poster_path
+    return full_path
 
 app = Flask(__name__)
 
