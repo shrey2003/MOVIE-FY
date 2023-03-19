25ce0dd0ee263ea7c31c385825911ff7e36ee06b
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-03-19 19:40:50 +0530

    updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import requests
 import pandas as pd
 from patsy import dmatrices
 
-movies=pickle.load(open('model/movies.pkl','rb'))
+movies=pickle.load(open('movies.pkl','rb'))
+similar=pickle.load(open('similarity.pkl','rb'))
 def poster(movie_id):
     url="https://api.themoviedb.org/3/movie/{}?api_key=906b44d502c9c4a91034067e114671a3",format(movie_id)
     data=requests.get(url)
@@ -12,6 +13,12 @@ def poster(movie_id):
     poster_path=data['poster_path']
     full_path="https://image.tmdb.org/t/p/w500/"+ poster_path
     return full_path
+
+def recommend(movie):
+    movie_index=movies[new_movies['title']==movie].index[0]
+    distances=cos_sim[movie_index]
+    movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    for i in movies_list:
 
 app = Flask(__name__)
 
