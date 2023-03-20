f6df1ef6f67fb6245bfdca53f3fa559a7cbac311
sanskriti242 <59368657+shrey2003@users.noreply.github.com>
2023-03-21 03:08:51 +0530

    Updated

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, render_template,request
+
+import bz2
 import pickle
+import compress_pickle
+from compress_pickle import dump, load
 import requests
 import pandas as pd
 from patsy import dmatrices
 
-movies=pickle.load(open('movies.pkl','rb'))
-similar=pickle.load(open('similarity.pkl','rb'))
+
+movies=compress_pickle.load(open('movies.pkl','rb'))
+similar=compress_pickle.load(open('similarity.pkl','rb'))
 def poster(movie_id):
     url="https://api.themoviedb.org/3/movie/{}?api_key=906b44d502c9c4a91034067e114671a3",format(movie_id)
     data=requests.get(url)
@@ -14,7 +19,7 @@ def poster(movie_id):
     full_path="https://image.tmdb.org/t/p/w500/"+ poster_path
     return full_path
 
-def recommend(movie):
+def recommended(movie):
     movie_index=movies[movies['title']==movie].index[0]
     distances=similar[movie_index]
     movies_list=sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
@@ -25,7 +30,7 @@ def recommend(movie):
         recommended_movieposter.append(poster(movie_id))
         recommended_moviename.append(movies.iloc[i[0]].title)
 
-    return recommended_moviename,recommended_movieposter
+        return recommended_moviename,recommended_movieposter
 
 app = Flask(__name__)
 
@@ -33,21 +38,26 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/recommend',methods=['GET','POST'])
-def recommend():
+@app.route('/recommendation',methods=['GET','POST'])
+def recommendation():
     movie_list=movies['title'].values
     if request.method =="POST":
         try:
             if request.form:
                 movies_name=request.form['movies']
-                #print(movies_name)
-                recommended_moviename,recommended_movieposter=recommend(movies_name)
+                print(movies_name)
+                recommended_moviename,recommended_movieposter=recommended(movies_name)
                 return render_template("recommend.html",movies_name=recommended_moviename,poster=recommended_movieposter,movie_list=movie_list)
         except Exception as e:
             error={'error':e}
-            return render_template("recommend.html")
+            return render_template("recommend.html",error=error,movie_list=movie_list)
     else:
-        return render_template("recommend.html")
+        return render_template("recommend.html",movie_list=movie_list)
 
 
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
      
